60b928ea46452aa8b4fcc8192481aef33580f81a
valentinavargasp <valentinapescarav@gmail.com>
2025-05-09 20:33:56 -0300

    Agregar métodos para obtener un usuario por ID y guardar un nuevo usuario en UserController y UserService

--- src/main/java/com/alkemy/wallet/repository/UserRepository.java
+++ src/main/java/com/alkemy/wallet/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.alkemy.wallet.repository;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alkemy.wallet.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    List<User> findAll();
+
 }
