dcc2e1500493a643d885f59d73643d37dc51727b
LucianaAlzuri <alzuriluciana@gmail.com>
2025-05-10 15:01:44 -0300

    Modificación de personController, Repository, servicio e implementación

--- src/main/java/com/alkemy/wallet/repository/PersonRepository.java
+++ src/main/java/com/alkemy/wallet/repository/PersonRepository.java
@@ -1,6 +1,7 @@
 package com.alkemy.wallet.repository;
 
 import com.alkemy.wallet.models.Person;
+import java.util.List;
 
 import java.util.Optional;
 
@@ -8,4 +9,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person,Integer> {
     Optional<Person> findByName(String name);
+    Optional<Person> findByLastName(String lastName);
+    Optional<Person> findByPhoneNumber(String phoneNumber);
+    List<Person> findByLocationIgnoreCase(String location);
+    Optional<Person> findByAddress(String address);
+    Optional<Person> findByIdentityCard(int identityCard);
+    Optional<Person> findByDateBirth(String dateBirth);
 }
