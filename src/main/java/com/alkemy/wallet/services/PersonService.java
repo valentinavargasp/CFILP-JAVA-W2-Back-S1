1e0478f032fdf606109a247d94bdeaa192b2f3c2
teshy18 <sebas.tesitore@gmail.com>
2025-05-09 22:15:00 -0300

    crea metodos en las interfaces de servicio

--- src/main/java/com/alkemy/wallet/services/PersonService.java
+++ src/main/java/com/alkemy/wallet/services/PersonService.java
@@ -1,4 +1,16 @@
 package com.alkemy.wallet.services;
 
+import com.alkemy.wallet.models.Person;
+
 public interface PersonService {
+    //TODO: metodos para agregar, editar, borrar y  buscar personas(por id, documento, apellido?)
+
+    public void savePerson(Person person);
+
+    public void editPerson(Person person);
+
+    public void deletePersonById(int id);
+
+    public Person findPersonById(int id);
+    //todo:  buscar personas por apellido, dni.
 }
--- src/main/java/com/alkemy/wallet/services/RoleService.java
+++ src/main/java/com/alkemy/wallet/services/RoleService.java
@@ -1,4 +1,7 @@
 package com.alkemy.wallet.services;
 
 public interface RoleService {
+
+    //TODO: crear metodos para agregar, editar , buscar y  borrar roles.
+
 }
