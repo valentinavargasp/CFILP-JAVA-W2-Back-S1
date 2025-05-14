10c04e27f679540b17cba816f119ae6fef3b9718
valentinavargasp <valentinapescarav@gmail.com>
2025-05-14 00:53:12 -0300

    refactor: agregar atributos a las clases de Transaction, se agrega Enum de TransactionMethod

--- src/main/java/com/alkemy/wallet/models/transaction/Deposit.java
+++ src/main/java/com/alkemy/wallet/models/transaction/Deposit.java
@@ -1,11 +1,27 @@
 package com.alkemy.wallet.models.transaction;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Entity
+@Table(name = "deposit")
+@PrimaryKeyJoinColumn(name = "id_transaction") // clave heredada de Transaction
 public class Deposit extends Transaction {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_deposit")
-    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "method")
+    private TransactionMethodEnum method;
+
+    @Column(name = "source_entity")
+    private String sourceEntity;
+
 }
