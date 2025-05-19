bf8489bc7251fb3323d864325d29daf675c66025
valentinavargasp <valentinapescarav@gmail.com>
2025-05-19 10:50:41 -0300

    Agregar soporte para MapStruct: se añade la dependencia y configuración en pom.xml, se implementa UserMapper y se corrige la URL de la base de datos en application.properties.

--- src/main/java/com/alkemy/wallet/dto/UserDTO.java
+++ src/main/java/com/alkemy/wallet/dto/UserDTO.java
@@ -1,5 +1,13 @@
 package com.alkemy.wallet.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
 
+    private int id;
+    private String username;
+    private String email;
+    private String fullName; //Podríamos formarlo acá a partir de la entidad persona
+    
 }
