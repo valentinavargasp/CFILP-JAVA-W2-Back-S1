72d631ae180407971aba5aaf918bb5e40caa738a
valentinavargasp <valentinapescarav@gmail.com>
2025-05-20 10:27:11 -0300

    Actualizar configuración de Maven para usar Java 17 y versiones más recientes de MapStruct y Lombok. Agregar pruebas para mapeadores de User y Person, y eliminar pruebas obsoletas de servicios.

--- src/main/java/com/alkemy/wallet/mapper/PersonMapper.java
+++ src/main/java/com/alkemy/wallet/mapper/PersonMapper.java
@@ -3,7 +3,6 @@ package com.alkemy.wallet.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import com.alkemy.wallet.dto.PersonDTO;
 import com.alkemy.wallet.models.user.Person;
@@ -15,13 +14,6 @@ import com.alkemy.wallet.models.user.Person;
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
 
-    /**
-     * Instancia singleton de la interfaz PersonMapper, creada usando la fábrica
-     * Mappers de MapStruct.
-     * Esta instancia proporciona métodos de mapeo para convertir entre entidades
-     * Person y sus DTOs.
-     */
-    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
     /**
      * Convierte una entidad Person en su DTO.
