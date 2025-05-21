ce806ae88dfcfeeda17595770a971199153bd820
teshy18 <sebas.tesitore@gmail.com>
2025-05-20 22:56:55 -0300

    cambios en userCreateServiceImpl...

--- src/main/java/com/alkemy/wallet/services/user/impl/UserCreateServiceImpl.java
+++ src/main/java/com/alkemy/wallet/services/user/impl/UserCreateServiceImpl.java
@@ -1,14 +1,20 @@
 package com.alkemy.wallet.services.user.impl;
 
+import java.util.List;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.alkemy.wallet.dto.UserCreateDTO;
 import com.alkemy.wallet.mapper.UserMapper;
 import com.alkemy.wallet.models.user.Person;
+import com.alkemy.wallet.models.user.Role;
 import com.alkemy.wallet.models.user.User;
+import com.alkemy.wallet.models.user.UserRole;
 import com.alkemy.wallet.repository.user.PersonRepository;
+import com.alkemy.wallet.repository.user.RoleRepository;
 import com.alkemy.wallet.repository.user.UserRepository;
+import com.alkemy.wallet.repository.user.UserRoleRepository;
 import com.alkemy.wallet.services.user.UserCreateService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +27,8 @@ public class UserCreateServiceImpl implements UserCreateService {
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
     private final PersonRepository personRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final RoleRepository roleRepository;
 
     /**
      * Guarda un nuevo usuario a partir de un DTO.
@@ -43,16 +51,27 @@ public class UserCreateServiceImpl implements UserCreateService {
             //Se guarda el usuario con la contraseña encriptada y el id de la persona
             User user = userMapper.toEntity(userCreateDTO);
             user.setPerson(person);
-
             user.setPassword(passwordEncoder.encode(userCreateDTO.getPassword()));
             User savedUser = userRepository.save(user);
 
-            System.out.println("......");
-            System.out.println("User saved: " + savedUser);
-            System.out.println("......");
+            System.out.println("Se guardo la persona: " + person);
+            System.out.println("Se guardo el usuario: " + savedUser);
+            //Se guarda el rol del usuario
+            //EL id 6 es el id del rol de usuario "CLIENTE"
+            Role role = roleRepository.findById(6).orElseThrow(() -> new IllegalArgumentException("Rol no encontrado"));
+            System.out.println("ROL:" + role);
+            UserRole userRole = userRoleRepository.save(new UserRole(savedUser, role));
+            System.out.println("Se guardo el rol: " + userRole);
+            
+            //Se guarda el usuario con el rol
+            savedUser.setUserRoles(List.of(userRole));
+            System.out.println("----------------------------------");
+            System.out.println("Se guardo el usuario con el rol: " + savedUser);
+            savedUser = userRepository.save(savedUser);
 
-            //TODO: Debería guardar el rol del usuario aquí con savedUser.getId()
 
+            System.out.println("Se guardo el usuario: " + savedUser);    
+            
             return userMapper.toCreateDTO(savedUser);
         } catch (Exception e) {
             throw new RuntimeException(e);
