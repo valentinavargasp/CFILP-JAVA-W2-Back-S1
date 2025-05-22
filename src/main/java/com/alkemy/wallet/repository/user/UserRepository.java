d0ac56ffd5102d80cff6d591c46745d2de34f9bc
teshy18 <sebas.tesitore@gmail.com>
2025-05-22 06:43:41 -0300

    Refactor: se eliminan lineas vacias.

--- src/main/java/com/alkemy/wallet/repository/user/UserRepository.java
+++ src/main/java/com/alkemy/wallet/repository/user/UserRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import com.alkemy.wallet.models.user.User;
-
 import java.util.List;
 import java.util.Optional;
 
